97dcb18c5d9fbad476ab586594db4df4319a4ba5
Michael <mnovik4@gmail.com>
2022-08-12 20:40:59 +0300

    Добавление кода программы

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿string [] Symbols = new string [5]
+{
+    "sjkdfl", "123", "igj", "poemr", "1"
+};
+
+void GetArray(string[] array)
+{
+    string count;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length<=3)
+        {
+            count = array[i];
+            Console.Write(count + " ");
+        }
+    }
+    Console.WriteLine();
+}
+
+GetArray(Symbols);
+
